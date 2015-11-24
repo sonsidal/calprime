f55cc31820af3a2cab4d9c752a8f8221df3d7e76
sonsidal <sonsidal@gmail.com>
2015-11-25 01:23:27 +0900

    complete algorithm using dynamic programming

--- prime.c
+++ prime.c
@@ -10,14 +10,14 @@ int isPrime(int x){
 	int i;
 	int count=0;
 	for(i=0; i<offset; i++){
-		if(x%primeset[i] == 0)	count++;
+		if(x%primeset[i] == 0){
+			return 0;
+		}
 	}
+	
 	return 1;
 }
 
-void insert_into_tab(int x){
-
-}
 
 int main(){
 	int i;
@@ -26,7 +26,11 @@ int main(){
 	primeset[offset++] = 2;
 
 	for(i=3; i<NUM; i++){
-		if(isPrime(i) == 1)	insert_into_tab(i);
+		if(isPrime(i) == 1)	primeset[offset++] = i;
 	}
 	
+	for(i=0; i<offset; i++){
+		printf("%d\n", primeset[i]);
+	}
+
 }
