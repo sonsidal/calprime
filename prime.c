f3c07e950fda8b09f3ed359bd8479e857cd06345
sonsidal <sonsidal@gmail.com>
2015-11-25 14:33:01 +0900

    added write and read function

--- prime.c
+++ prime.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #define NUM 100
 #define SIZE_OF_TAB 100
+#define RANGE 0
+#define POINT 1
 
 int *primeset;
 int offset;
@@ -18,19 +20,51 @@ int isPrime(int x){
 	return 1;
 }
 
+//for now, write from first of the file
+void writePrime(){
+	int i;
+	FILE *fp = fopen("primes.txt", "w");
+	fprintf(fp, "%d\n", offset);
+	
+	for(i=0; i<offset; i++){
+		fprintf(fp, "%d ", primeset[i]);
+	}
+
+	fclose(fp);
+}
+
+void readPrime(){
+	int i;
+	FILE *fp = fopen("primes.txt", "r");
+	fscanf(fp, "%d\n", &offset);
+	for(i=0; i<offset; i++){
+		fscanf(fp, "%d", &primeset[i]);
+	}
+}
 
 int main(){
 	int i;
+	char c;
+	int num;	//To store the number to check
 
 	primeset = (int*)malloc(sizeof(int)*SIZE_OF_TAB);
 	primeset[offset++] = 2;
-
-	for(i=3; i<NUM; i++){
+	
+	while(1){
+		printf("Enter the number to check if it is prime.\n");
+		scanf("%d", &num);
+		if(num > 3)	break;
+		printf("please enter the number greater than 2\n");
+	}
+	
+	for(i=3; i<num; i++){
 		if(isPrime(i) == 1)	primeset[offset++] = i;
 	}
 	
 	for(i=0; i<offset; i++){
 		printf("%d\n", primeset[i]);
 	}
+
+	writePrime();
 
 }
